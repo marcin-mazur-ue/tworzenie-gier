af75d40b6657fa21d265bd0c224ecb18d2e1fec2
Wojciech Leś <118069461+Rivellan@users.noreply.github.com>
2022-12-13 23:56:43 +0100

    Modele i animacje
    Dodanie modeli gracza i zombie oraz przykładowych animacji. Atak zombie ma teraz opóźnienie. Pliki z assetami trzeba przenieść w odpowiednie miejsca i usunąć niepotrzebną resztę.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,122 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Enemy : MonoBehaviour
+public class Enemy : EnemyBase
 {
-	[SerializeField] private float speed = 2.0f;
-	[SerializeField] private int health = 20;
-	[SerializeField] private int attack_strength = 20;
-	[SerializeField] private float attack_cooldown = 2.0f;
-	[SerializeField] private float attack_range = 1.0f;
-	private int current_health;
-	private float current_attack_cooldown;
-
-	private Rigidbody rb;
-	private Transform player_position;
-	private float direction;
-
-	[SerializeField] private BoxCollider attack_area;
-	[SerializeField] private Slider health_bar;
-
+	
 	void Start()
 	{
-		rb = GetComponent<Rigidbody>();
-		attack_area.transform.localScale = new Vector3(1.0f, 1.0f, attack_range);
-		attack_area.transform.localPosition = new Vector3(0.0f, 0.0f, 0.5f * (attack_range + 1.0f));
-		player_position = FindObjectOfType<Player>().GetComponent<Transform>();
-		direction = 1.0f;
-		current_health = health;
-		current_attack_cooldown = 0.0f;
-		update_health_bar();
+		initialization();
 	}
 
 	void FixedUpdate()
 	{
-		Vector3 movement_vector = player_position.position - transform.position;
-		Vector3 movement_vector_normalized = new Vector3(movement_vector.x / Mathf.Abs(movement_vector.x), 0.0f, movement_vector.z / Mathf.Abs(movement_vector.z));
-		if(float.IsNaN(movement_vector_normalized.x))
-			movement_vector_normalized.x = 0.0f;
-		if(float.IsNaN(movement_vector_normalized.z))
-			movement_vector_normalized.z = 0.0f;
-		if(movement_vector_normalized.z * direction < 0.0f)
-		{
-			Vector3 rotation = new Vector3(0.0f, 180.0f, 0.0f);
-			transform.Rotate(rotation);
-			health_bar.transform.Rotate(rotation);
-			direction = movement_vector_normalized.z;
-		}
-		rb.velocity = transform.rotation * new Vector3(movement_vector_normalized.x, 0.0f, movement_vector_normalized.z) * direction * speed;
+		movement();
 	}
 
 	void Update()
 	{
-		if(is_attack_on_cooldown() == true)
-		{
-			if(current_attack_cooldown <= Time.deltaTime)
-				current_attack_cooldown = 0.0f;
-			else
-				current_attack_cooldown -= Time.deltaTime;
-		}
-		if(can_attack_player() == true)
-			attack();
+		attack_check();
 	}
 
-	public void receive_damage(int amount)
-	{
-		current_health -= amount;
-		if(current_health <= 0)
-		{
-			current_health = 0;
-			update_health_bar();
-			die();
-		}
-		else
-			update_health_bar();
-	}
 
-	private void attack()
-	{
-		current_attack_cooldown = attack_cooldown;
 
-		Collider[] attack_area_colliders = Physics.OverlapBox(attack_area.transform.position, attack_area.transform.lossyScale / 2.0f, Quaternion.identity);
-		foreach(Collider collider in attack_area_colliders)
-		{
-			if(collider.gameObject.tag == "Player")
-				collider.gameObject.GetComponent<Player>().receive_damage(attack_strength);
-		}
-	}
-
-	private bool can_attack_player()
-	{
-		if(is_attack_on_cooldown() == true)
-			return false;
-
-		Collider[] attack_area_colliders = Physics.OverlapBox(attack_area.transform.position, attack_area.transform.lossyScale / 2.0f, Quaternion.identity);
-		foreach(Collider collider in attack_area_colliders)
-		{
-			if(collider.gameObject.tag == "Player")
-				return true;
-		}
-		return false;
-	}
-
-	private void die()
-	{
-		Destroy(gameObject);
-	}
-	
-	private float get_health_percentage()
-	{
-		return (float)(current_health) / (float)(health);
-	}
-
-	private bool is_attack_on_cooldown()
-	{
-		return current_attack_cooldown > 0.0f;
-	}
-	
-	private void update_health_bar()
-	{
-		health_bar.value = get_health_percentage();
-	}
 }
