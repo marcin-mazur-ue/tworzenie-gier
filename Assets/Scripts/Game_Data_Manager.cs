3357e385e5f63f8e59afe2463aa3f7ed842969e6
marcin-mazur-ue <marcin.mazur3@edu.uekat.pl>
2023-01-09 02:13:42 +0100

    Wczytywanie gry
    Działający ekran wczytywania gry, wyświetlający postępy na danym zapisie. Aktualnie obsługuje 4 sloty na zapisy, wszystkie o ustalonych z góry ścieżkach.

--- Assets/Scripts/Game_Data_Manager.cs
+++ Assets/Scripts/Game_Data_Manager.cs
@@ -20,18 +20,26 @@ public class Game_Data_Manager : MonoBehaviour
 		}
 	}
 	
-	public static void load_from_file(int save_slot_index)
+	public static void load_and_apply(int save_slot_index)
+	{
+		Game_Data loaded_data = load_from_file(save_slot_index);
+		if (loaded_data == null)
+			Debug.LogError("Nie można wczytać pliku \"" + get_save_file_path(save_slot_index) + "\"!"); //TODO okno z komunikatem o błędzie zamiast tego
+		else
+			instance.data = loaded_data;
+	}
+	
+	public static Game_Data load_from_file(int save_slot_index)
 	{
 		string file_path = get_save_file_path(save_slot_index);
-		if (File.Exists(file_path))
-		{
-			BinaryFormatter formatter = new BinaryFormatter();
-			FileStream file = File.Open(file_path, FileMode.Open);
-			instance.data = (Game_Data)formatter.Deserialize(file);
-			file.Close();
-		}
-		else
-			Debug.LogError("Nie można otworzyć pliku \"" + file_path + "\"!"); //TODO okno z komunikatem o błędzie zamiast tego
+		if (File.Exists(file_path) == false)
+			return null;
+		
+		BinaryFormatter formatter = new BinaryFormatter();
+		FileStream file = File.Open(file_path, FileMode.Open);
+		Game_Data loaded_data = (Game_Data)(formatter.Deserialize(file));
+		file.Close();
+		return loaded_data;
 	}
 	
 	public static void save_to_file(int save_slot_index)
@@ -54,14 +62,14 @@ public class Game_Data_Manager : MonoBehaviour
 }
 
 [Serializable]
-class Game_Data
+public class Game_Data
 {
-	private const int levels_amount = 2;
-	private const int stars_amount = 3;
-	private const int weapons_amount = 3;
+	public const int levels_amount = 2;
+	public const int stars_per_level = 3;
+	public const int weapons_amount = 3;
 	
 	public bool[] levels_unlocked = new bool[levels_amount - 1]; // pierwszy poziom jest zawsze odblokowany, więc levels_unlocked[0] oznacza drugi, levels_unlocked[1] trzeci itd.
-	public bool[,] stars_gained = new bool[levels_amount, stars_amount];
+	public bool[,] stars_gained = new bool[levels_amount, stars_per_level];
 	public bool[] weapons_unlocked = new bool[weapons_amount - 1]; // "brak broni" liczy się jako broń, ale jest zawsze odblokowany
 	
 	public Game_Data()
@@ -75,5 +83,45 @@ class Game_Data
 		}
 		for (int i = 0; i < weapons_unlocked.Length; i++)
 			weapons_unlocked[i] = false;
+	}
+	
+	public float get_game_completion_percentage()
+	{
+		return (float)(get_stars_gained_amount() + get_weapons_unlocked_amount()) / (float)(levels_amount * stars_per_level + weapons_amount);
+	}
+	
+	public int get_highest_unlocked_level()
+	{
+		for (int i = 0; i < levels_unlocked.Length; i++)
+		{
+			if (levels_unlocked[i] == true)
+				return i + 2;
+		}
+		return 1;
+	}
+	
+	public int get_stars_gained_amount()
+	{
+		int stars_gained_amount = 0;
+		for (int i = 0; i < stars_gained.GetLength(0); i++)
+		{
+			for (int j = 0; j < stars_gained.GetLength(1); j++)
+			{
+				if (stars_gained[i, j] == true)
+					stars_gained_amount++;
+			}
+		}
+		return stars_gained_amount;
+	}
+	
+	public int get_weapons_unlocked_amount()
+	{
+		int weapons_unlocked_amount = 1;
+		for (int i = 0; i < weapons_unlocked.Length; i++)
+		{
+			if (weapons_unlocked[i] == true)
+				weapons_unlocked_amount++;
+		}
+		return weapons_unlocked_amount;
 	}
 }
---
+++ Assets/Scripts/UI/Load_Save_Panel.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Load_Save_Panel : MonoBehaviour
+{
+	[SerializeField] private int save_slot_index;
+	[SerializeField] private GameObject panel_save_info;
+	[SerializeField] private Text completion_percentage;
+	[SerializeField] private Text highest_level_unlocked;
+	[SerializeField] private Text stars_gained;
+	[SerializeField] private Text weapons_unlocked;
+	
+	private Game_Data data;
+	
+	void Start()
+	{
+		data = Game_Data_Manager.load_from_file(save_slot_index);
+		if (data == null)
+		{
+			panel_save_info.SetActive(false);
+			return;
+		}
+		completion_percentage.text = (Mathf.Floor(data.get_game_completion_percentage() * 100.0f)).ToString() + "%";
+		highest_level_unlocked.text = "Poziom " + data.get_highest_unlocked_level().ToString();
+		stars_gained.text = data.get_stars_gained_amount().ToString().PadLeft(2, ' ') + "/" + (Game_Data.levels_amount * Game_Data.stars_per_level).ToString();
+		weapons_unlocked.text = data.get_weapons_unlocked_amount().ToString().PadLeft(2, ' ') + "/" + Game_Data.weapons_amount.ToString();
+	}
+}
