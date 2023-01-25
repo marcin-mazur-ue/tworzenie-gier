8ad3593e517edde5c89cfecedf17f79e8ef447f2
marcin-mazur-ue <marcin.mazur3@edu.uekat.pl>
2023-01-25 01:07:48 +0100

    Ustawienia graficzne
    Menu ustawień graficznych, pozwalające na zmianę rozdzielczości oraz na przełączanie pełnego ekranu i synchronizacji pionowej.

--- Assets/Scripts/UI/Main_Menu.cs
+++ Assets/Scripts/UI/Main_Menu.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -9,7 +10,42 @@ public class Main_Menu : MonoBehaviour
 	[SerializeField] private GameObject achievements_panel;
 	[SerializeField] private GameObject settings_panel;
 	
+	[SerializeField] private Toggle fullscreen_toggle;
+	[SerializeField] private Toggle vsync_toggle;
+	[SerializeField] private Dropdown resolutions_dropdown;
+	
+	private List<Resolution> resolutions = new List<Resolution>();
 	private const int level_select_scene_id = 1;
+	
+	void Start()
+	{
+		fullscreen_toggle.isOn = Screen.fullScreen;
+		vsync_toggle.isOn = (QualitySettings.vSyncCount != 0);
+		List<string> resolution_strings = new List<string>();
+		int active_resolution_index = -1;
+		for (int i = 0; i < Screen.resolutions.Length; i++)
+		{
+			Resolution current_resolution = Screen.resolutions[i];
+			resolutions.Add(current_resolution);
+			resolution_strings.Add(current_resolution.width + "x" + current_resolution.height + " " + current_resolution.refreshRate + "Hz");
+			if (current_resolution.width == Screen.width && current_resolution.height == Screen.height && current_resolution.refreshRate == Screen.currentResolution.refreshRate)
+				active_resolution_index = i;
+		}
+		resolutions_dropdown.ClearOptions();
+		resolutions_dropdown.AddOptions(resolution_strings);
+		resolutions_dropdown.value = active_resolution_index;
+		resolutions_dropdown.RefreshShownValue();
+	}
+	
+	public void apply_settings()
+	{
+		if (vsync_toggle.isOn == true)
+			QualitySettings.vSyncCount = 1;
+		else
+			QualitySettings.vSyncCount = 0;
+		Resolution active_resolution = resolutions[resolutions_dropdown.value];
+		Screen.SetResolution(active_resolution.width, active_resolution.height, fullscreen_toggle.isOn);
+	}
 	
 	public void load_save(int save_slot_id)
 	{
